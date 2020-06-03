5de97cda128faac4451b2785f41c4b85401b7ba8
Filipe Varjão <varjaofilipe@gmail.com>
2020-06-03 17:45:38 -0300

    [add] interval definition and operation

---
+++ lib/ex_interval/rounding.ex
@@ -0,0 +1,11 @@
+defmodule ExInterval.Rounding do
+  @on_load :load_nifs
+
+  def load_nifs, do: :erlang.load_nif('./rounding', 0)
+
+  def init, do: :erlang.load_nif('./rounding', 0)
+
+  def get_mode, do: raise "NIF get_mode/0 not implemented"
+
+  def set_mode(_), do: raise "NIF set_mode/0 not implemented"
+end
