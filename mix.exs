ab7e8fe0abb4149c4167bf5606f2e2ecf3e0e51f
Filipe Varjão <varjaofilipe@gmail.com>
2020-06-22 16:30:34 -0300

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExInterval.MixProject do
   def project do
     [
       app: :ex_interval,
-      version: "0.1.0",
+      version: "0.1.3",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers() ++ [:ex_interval],
